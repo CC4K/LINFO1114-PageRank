eccbe5980990f88e702f4d52185d2b297a42aae8
Cédric Kheirallah <cedric.kheirallah@student.uclouvain.be>
2022-05-14 15:37:34 +0200

    corrections terminées + verbose

--- pageRank.py
+++ pageRank.py
@@ -18,9 +18,11 @@ def pageRankLinear(A, v, alpha=0.9):
         A[i] /= np.sum(A[i])
     P = A.T
 
-    # Résoudre le système linéaire creux
+    # Résoudre le système linéaire creux (I - alpha*P) . x = (1-alpha)*v
+    # cfr cours page 144
     I = np.eye(len(A), len(A))
-    x = np.linalg.solve(I - (alpha * P), (1-alpha) * v)
+    x = np.linalg.solve(I - (alpha*P), (1-alpha)*v)
+    if verbose: print("Système linéaire :\n", I - (alpha*P), "\n", (1-alpha)*v, sep="")
 
     return x
 
@@ -32,28 +34,30 @@ def pageRankPower(A, v, alpha=0.9):
     :param alpha: paramètre de téléportation α compris entre 0 et 1, α ∈]0,1[ (0.9 par défaut et pour les résultats à présenter)
     :return x: un vecteur contenant les scores d’importance des nœuds ordonnés dans le même ordre que la matrice d’adjacence.
     """
-    print("Matrice d'adjacence A =\n", A, sep="")
+    if verbose: print("Matrice d'adjacence A =\n", A, sep="")
 
     # Matrice de probabilités de transition P
     for i in range(len(A)):
         A[i] /= np.sum(A[i])
     P = A.T
-    print("Matrice de probabilités de transition P =\n", P, sep="")
+    if verbose: print("Matrice de probabilités de transition P =\n", P, sep="")
 
     # Itérer sur la matrice G
-    x = np.ones(len(A))
-    G = (alpha*P) + (1 - alpha)/len(A) # http://www.scholarpedia.org/article/Google_matrix
-    print("Matrice Google G =\n", G, sep="")
-    for i in range(190):
-        x = ((alpha * P) @ x) + ((1-alpha) * v)
-        if i < 3:
-            print("Vecteur de scores à l'itération ", i, " =\n", x, sep="")
+    # https://mathstats.uncg.edu/sites/yasaki/publications/machado-google-pagerank-linear-algebra-project.pdf
+    x = v/sum(v)
+    G = (alpha*P) + (1-alpha)/len(A)  # http://www.scholarpedia.org/article/Google_matrix
+    if verbose: print("Matrice Google G =\n", G, sep="")
+    for i in range(200):
+        x = G @ x
+        if (i < 3) and verbose: print("Vecteur de scores à l'itération ", i + 1, " =\n", x, sep="")
 
     return x
 
-
 if __name__ == '__main__':
-    np.set_printoptions(linewidth=120)
+    np.set_printoptions(linewidth=150)
+
+    # Verbose, changez à True pour afficher les étapes demandées pour le projet
+    verbose = False
 
     # Lecture de la matrice d'adjacence A à partir d'un fichier .csv
     file = open('Adjacency_matrix_A.csv')
@@ -76,4 +80,4 @@ if __name__ == '__main__':
     print("Vecteur de scores x =\n", pageRankLinear(np.copy(A), np.copy(v), alpha), sep="")
     print("===============================================================================================================")
     print("pageRankPower :")
-    print("Vecteur de scores x final =\n", pageRankPower(np.copy(A), np.copy(v), alpha), sep="")
+    print("Vecteur de scores x =\n", pageRankPower(np.copy(A), np.copy(v), alpha), sep="")
